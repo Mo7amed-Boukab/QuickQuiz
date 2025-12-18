ea5baff39d77bd5e658d6597b892779db41dab97
mohamed <mohamedboukab202@gmail.com>
2025-12-18 15:35:39 +0100

    feat: Initialize app with React+ tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+   <div className="flex flex-col items-center justify-center h-screen">
+    <button className="bg-gray-900 text-white py-2 px-4 rounded mt-4">Start Quiz</button>
+   </div>
+  )
+}
+
+export default App
